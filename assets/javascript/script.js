cd3dbb1da84fce28c3821a5ee3d5e549766008a2
Juliette Rapala <jrapala@gmail.com>
2017-10-11 14:41:29 -0500

    Code cleanup.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,116 +1,123 @@
 // Train Scheduler | By Juliette Rapala
 // =====================================================================================
 
-
-
 $(document).ready(function(){
 
 	// Setup Variables 
 	// =====================================================================================
 
-	// Initialize Firebase
-	var config = {
-	    apiKey: "AIzaSyBxnnRtSx7b0R0FbBdGBk-F8Ygh9dqz--I",
-	    authDomain: "train-scheduler-67666.firebaseapp.com",
-	    databaseURL: "https://train-scheduler-67666.firebaseio.com",
-	    projectId: "train-scheduler-67666",
-	    storageBucket: "",
-	    messagingSenderId: "357498440689"
-	};
+		// Initialize Firebase
+		var config = {
+		    apiKey: "AIzaSyBxnnRtSx7b0R0FbBdGBk-F8Ygh9dqz--I",
+		    authDomain: "train-scheduler-67666.firebaseapp.com",
+		    databaseURL: "https://train-scheduler-67666.firebaseio.com",
+		    projectId: "train-scheduler-67666",
+		    storageBucket: "",
+		    messagingSenderId: "357498440689"
+		};
 
-	firebase.initializeApp(config);
-	var database = firebase.database();
+		firebase.initializeApp(config);
+		var database = firebase.database();
 
-	var trainName = '';
-	var destination = '';
-	var firstTrainTime = '';
-	var frequency = 0;
-	var nextArrival = 0;
-	var minutesAway = 0;
+		// Data varaiables
+		var trainName = '';
+		var destination = '';
+		var firstTrainTime = '';
+		var frequency = 0;
+		var nextArrival = 0;
+		var minutesAway = 0;
 
-	// Get current time in moment.js format
-	var currentTime = moment();
+		// Get current time in moment.js format
+		var currentTime = moment();
 
-	// Train icon file
-	var trainIcon = "<img src='assets/images/trainIcon.svg' height='20px'>"
+		// Train icon file
+		var trainIcon = "<img src='assets/images/trainIcon.svg' height='20px'>"
 
-	// RegEx for page reload
-	var re = new RegExp("[5][9]$");
-
-	// Clock Function
-	function update() {
-		var clockTime = moment().format('HH:mm:ss');
-  		$('#clock').html(clockTime);
-  		// If time ends in 59 seconds, reload page
-  		if (re.test(clockTime)) {
-    		location.reload();	
-		}
-	}
-	setInterval(update, 1000);
+		// RegEx for clock function
+		var re = new RegExp("[0][0]$");
 
 	// Main Processes 
 	// =====================================================================================
 
-	// Update Welcome Message
-	var $welcomeMessage = $('<h3>');
-	$welcomeMessage.html("Welcome! The current time is <div id='clock' class='clock'></div>");
-	$('#welcomeMessage').append($welcomeMessage);
+		// Clock Function
+		function update() {
+			var clockTime = moment().format('hh:mm:ss');
+	  		$('#clock').html(clockTime);
+	  		// If time ends in 00 seconds, reload page
+	  		if (re.test(clockTime)) {
+	    		location.reload();	
+			}
+		}
+		setInterval(update, 1000);
 
-	//currentTime.format("hh:mm A")
+		// Update Welcome Message
+		var $welcomeMessage = $('<h3>');
+		$welcomeMessage.html("Welcome! The current time is <div id='clock' class='clock'></div>");
+		$('#welcomeMessage').append($welcomeMessage);
 
-	// Populate objects from database
-	database.ref().on("value", function(snapshot) {
+		// Populate objects from database
+		database.ref().on("value", function(snapshot) {
 
-		// Loop through values of each entry in the database and assign them to variables.
-		snapshot.forEach(function(childSnapshot){
-			trainName = childSnapshot.val().trainName;
-		    destination = childSnapshot.val().destination;
-		    frequency = childSnapshot.val().frequency;
-		    firstTrainTime = childSnapshot.val().firstTrainTime;
+			// Loop through values of each entry in the database and assign them to variables.
+			snapshot.forEach(function(childSnapshot){
+				trainName = childSnapshot.val().trainName;
+			    destination = childSnapshot.val().destination;
+			    frequency = childSnapshot.val().frequency;
+			    firstTrainTime = childSnapshot.val().firstTrainTime;
 
-		    // Convert to firstTrainTime to moment.js format
-		    var convertedFirstTrainTime = moment(firstTrainTime, 'HH:mm');
+			    // Convert to firstTrainTime to moment.js format
+			    var convertedFirstTrainTime = moment(firstTrainTime, 'HH:mm');
 
-		    // Calculate next train arrival
-		    for (var i = convertedFirstTrainTime; i.isBefore(currentTime); i.add(frequency, 'm')) {
-		    	nextArrival = i;
-		    }	
+			    // Calculate next train arrival
+			    for (var i = convertedFirstTrainTime; i.isBefore(currentTime); i.add(frequency, 'm')) {
+			    	nextArrival = i;
+			    }	
 
-		   	// Calculate minutes until next train
-		   	minutesAway = nextArrival.diff(currentTime, 'minutes');
-		   	minutesAway++;
+			   	// Calculate minutes until next train
+			   	minutesAway = nextArrival.diff(currentTime, 'minutes');
+			   	minutesAway++;
 
-		   	// Add values to table
+			   	// Add values to table
+			    $("tbody").append("<tr class='trainRecord'><td>" + 
+			    	trainIcon + " " + 
+			    	trainName + 
+			    	"</td><td>" + 
+			    	destination + 
+			    	"</td><td>" + 
+			    	frequency + 
+			    	"</td><td>" + 
+			    	nextArrival.format("hh:mm A") + 
+			    	"</td><td>" + 
+			    	minutesAway + 
+			    	"</td></tr>");
+			});
 
-		    $("tbody").append("<tr class='trainRecord'><td>" + trainIcon + " " + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival.format("hh:mm A") + "</td><td>" + minutesAway + "</td></tr>");
+
+	  	// If any errors are experienced, log them to console.
+		}, function(errorObject) {
+	  		console.log("The read failed: " + errorObject.code);
 		});
 
+		// Submit to database
+		$("#submitButton").on("click", function(event) {
 
-  	// If any errors are experienced, log them to console.
-	}, function(errorObject) {
-  		console.log("The read failed: " + errorObject.code);
-	});
+			event.preventDefault();
 
-	// Submit to database
-	$("#submitButton").on("click", function(event) {
+			// Clear current table
+			$('.trainRecord').empty();
 
-		event.preventDefault();
+			// Get values submitted via form
+			trainName = $("#trainName").val().trim();
+			destination = $("#destination").val().trim();
+			firstTrainTime = $("#firstTrainTime").val();
+			frequency = $("#frequency").val();
 
-		// Clear current table
-		$('.trainRecord').empty();
-
-		// Get values submitted via form
-		trainName = $("#trainName").val().trim();
-		destination = $("#destination").val().trim();
-		firstTrainTime = $("#firstTrainTime").val();
-		frequency = $("#frequency").val();
-
-		// Add values to database
-		database.ref().push({
-		  trainName: trainName,
-		  destination: destination,
-		  firstTrainTime: firstTrainTime,
-		  frequency : frequency
+			// Add values to database
+			database.ref().push({
+				trainName: trainName,
+				destination: destination,
+				firstTrainTime: firstTrainTime,
+				frequency : frequency
+			});
 		});
-	});
 });
